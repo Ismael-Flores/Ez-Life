7d3db5dbceeb2a26ca5e94435e195d00fe8e3de6
Ismael-Flores <issmmael10@gmail.com>
2022-06-24 00:37:20 -0700

    Note and User Database Model

---
+++ website/model.py
@@ -0,0 +1,18 @@
+from pytz import timezone
+from . import db
+from flask_login import UserMixin
+from sqlalchemy import func
+
+#Note Database Model
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+#USer Database Model
+class User(db.Model, UserMixin):
+    #Design database
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
