59b1634630e9f133e67120cb4020ddbc7e9c7317
Ismael-Flores <issmmael10@gmail.com>
2022-06-27 16:03:35 -0700

    flask login module

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import false
 from os import path
+from flask_login import LoginManager, login_manager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -14,6 +15,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+    
 
     from .views import views
     from .auth import auth
@@ -24,6 +26,14 @@ def create_app():
     from .models import User, Note
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
